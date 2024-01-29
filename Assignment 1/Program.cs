4defb87eee037118102c5046add45b372eabecbc
Pratyusha Jayavarapu <Pjayavarapu3016@conestogac.on.ca>
2024-01-28 23:39:52 -0500

    Created pet stimulator programs, worked on the pet creation part

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// pet stimulator
+// step1 here pet type is created and pet name will be given
+
+namespace PetStimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the PetStimulator Program!");
+            Console.WriteLine("Please choose your pet type:");
+            Console.WriteLine("1. Cat");
+            Console.WriteLine("2. Dog");
+            Console.WriteLine("3. Rabbit");
+            Console.WriteLine("4. Bird");
+            Console.Write("Enter the number of your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            string[] petTypes = { "Cat", "Dog", "Rabbit", "Bird" };
+
+            Console.Write("Enter the name for your pet: ");
+            string petName = Console.ReadLine();
+
+            Console.WriteLine($"Welcome, {petName} the {petTypes[choice - 1]}!");
+
+        }
+    }
+}
+
+
+
+
+
+
